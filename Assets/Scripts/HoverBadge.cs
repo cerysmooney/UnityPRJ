29f86b0f1739cae533599ccc13a3c2cee8b822a4
cerys mooney <Cerys2.mooney@live.uwe.ac.uk>
2023-04-21 16:27:53 +0100

    added stuff

---
+++ Assets/Scripts/HoverBadge.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class HoverBadge : MonoBehaviour
+{
+  
+     public GameObject Inspection;
+    public InspectionObject inspectionObj;
+    public int index;
+    void Update()
+    {
+    Ray ray = Camera.main.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+    RaycastHit hit;
+    if(GetComponent<Collider>().Raycast(ray, out hit, 20f)) 
+    {
+        
+        print("hover on" + gameObject.name); 
+        SceneManager.LoadScene("Badge", LoadSceneMode.Single);
+        if(Input.GetMouseButtonDown(0))
+        {
+            Inspection.SetActive(true);
+        }
+    }
+    }
+}
